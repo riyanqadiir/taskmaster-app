8c1852885335677e120c8f05f65c6a1bf14632c1
riyanqadiir <rayyanbhatti@gmail.com>
2025-11-19 03:21:09 +0500

    few changes were made

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ app.set("views", "views");
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173", "https://your-frontend-domain.vercel.app"],
+    origin: [
+    "http://localhost:5173",
+    process.env.FRONTEND_URL
+],
     credentials: true,
     exposedHeaders: ["authorization"],
 }));
@@ -27,14 +30,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/user", userRoute, userProfile)
 app.use("/tasks", taskRoute)
 
-if(!fs.existsSync(path.join(__dirname,"/public/temp"))){
-    fs.mkdir(path.join(__dirname,"/public/temp"),{ recursive: true },(err)=>{
-        if (err){
+if (!fs.existsSync(path.join(__dirname, "/public/temp"))) {
+    fs.mkdir(path.join(__dirname, "/public/temp"), { recursive: true }, (err) => {
+        if (err) {
             return console.log(err)
         }
     })
 }
 
-app.listen(process.env.PORT, () => {
-    console.log(`server is listening on port ${process.env.PORT}`)
-})
+if (!process.env.VERCEL) {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`server running on port ${process.env.PORT}`);
+    });
+}
+
+module.exports = app;
